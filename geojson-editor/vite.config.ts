2d71d6795e635b4848940b8eed059db11377c373
cocaine_coder <tracywang1997@outlook.com>
2024-11-06 13:07:38 +0800

    更新打包配置

--- geojson-editor/vite.config.ts
+++ geojson-editor/vite.config.ts
@@ -4,6 +4,7 @@ import vue from '@vitejs/plugin-vue';
 export default defineConfig({
     plugins: [vue()],
     root: 'geojson-editor',
+    base: './',
     optimizeDeps: {
         include: [
             `monaco-editor/esm/vs/language/json/json.worker`,
